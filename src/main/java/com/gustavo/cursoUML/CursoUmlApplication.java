5c90391c0519ed4c6537a25a6c32f8b5f5b9cb7e
Gustavo Costa <costa@usp.br>
2022-03-07 15:25:41 -0300

    Projeto criado

---
+++ src/main/java/com/gustavo/cursoUML/CursoUmlApplication.java
@@ -0,0 +1,13 @@
+package com.gustavo.cursoUML;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoUmlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoUmlApplication.class, args);
+	}
+
+}
